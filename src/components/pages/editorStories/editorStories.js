0dc7bb0a49a01ccfa01a8b775db08bdc95aa3b44
RoninHietanen <84194778+RoninHietanen@users.noreply.github.com>
2021-10-31 23:45:29 +1100

    adding some features to the profile page

--- src/components/pages/editorStories/editorStories.js
+++ src/components/pages/editorStories/editorStories.js
@@ -7,7 +7,14 @@ function Edit () {
             {/* <button class="publish-btn" type="submit">Publish</button> */}
             <button class="edit-btn" type="submit">Edit</button>
             <div className="story-edit-box">
-
+                <form>
+                    <input
+                        type="text"
+                        id="story"
+                        name="st"
+                        className="story-input"
+                    />
+                </form>
             </div>
         </div>
     );
--- src/components/pages/profile/profile.js
+++ src/components/pages/profile/profile.js
@@ -4,11 +4,33 @@ import './profile.css';
 function Profile () {
     return (
         <div>
+            <div>
+                <div>
+                    <button class="new-btn" type="submit">New Story</button>
+                    <button class="about-btn" type="submit">AboutMe</button>
+                </div>
+                <div className="about-card">
+                    <div className="card-text">
+                        <h3>About</h3>
+                    </div>
+                    <div className="about-card-content">
+                        <form>
+                            <input
+                                type="text"
+                                id="story"
+                                name="st"
+                                className="about-input"
+                            />
+                        </form>
+                    </div>
+                </div>
+            </div>
+
             <div className="comments-card">
                 <div className="card-text">
                     <h3>Comments</h3>
                 </div>
-                <div className="card-content">
+                <div className="comment-card-content">
                     
                 </div>
             </div>
@@ -17,7 +39,7 @@ function Profile () {
                 <div className="card-text">
                     <h3>Stories</h3>
                 </div>
-                <div className="card-content">
+                <div className="story-card-content">
                     
                 </div>
             </div>
